529e75cd32a049b9bfb50cae454a5018695f21f5
Facundo Llanos <76456958+facundollanos@users.noreply.github.com>
2023-04-28 15:59:11 -0300

    Add files via upload

---
+++ Ejercicioss/leccion 1/problemas/ejercicio1_2_2_2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+
+
+   int cantidad;
+
+   do{
+   cout<<"Ingresar un valor correspondiente al sueldo: ";
+   cin>>cantidad;
+   if(cantidad!=0){
+   int billetes_100 = cantidad / 100;
+       cantidad %= 100;
+
+       int billetes_50 = cantidad / 50;
+       cantidad %= 50;
+
+       int billetes_20 = cantidad / 20;
+       cantidad %= 20;
+
+       int billetes_10 = cantidad / 10;
+       cantidad %= 10;
+
+       int billetes_5 = cantidad / 5;
+       cantidad %= 5;
+
+       int billetes_2 = cantidad / 2;
+       cantidad %= 2;
+
+       int billetes_1 = cantidad;
+
+
+          cout << "Billetes de 100: " << billetes_100 << endl;
+          cout << "Billetes de 50: " << billetes_50 << endl;
+          cout << "Billetes de 20: " << billetes_20 << endl;
+          cout << "Billetes de 10: " << billetes_10 << endl;
+          cout << "Billetes de 5: " << billetes_5 << endl;
+          cout << "Billetes de 2: " << billetes_2 << endl;
+          cout << "Billetes de 1: " << billetes_1 << endl;
+   }
+
+   }while(cantidad!=0);
+
+
+   cout<<"Fin del programa"<<endl;
+
+   return 0;
+
+};
